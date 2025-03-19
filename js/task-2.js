307c69cc7ef59534be01a0e452beec922f2c3fa0
ivanka-haidai <ivankia01@ukr.net>
2025-03-19 20:04:27 +0200

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,15 @@
+function formatMessage(message, maxLength) {
+  if (message.length <= maxLength) {
+    return message;
+  } else {
+    const slisedMessage = message.slice(0, maxLength);
+    return `${slisedMessage}...`
+  }
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); 
+console.log(formatMessage("Curabitur ligula sapien", 23)); 
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); 
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); 
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); 
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
